9a69e3050d765fe91bc2119c660ae71971b4b83b
Marcin Struś <marcin.strus@makimo.pl>
2020-06-03 22:02:22 +0200

    Add volume block

--- blocks.h
+++ blocks.h
@@ -1,7 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-        {" ", "df -h | grep \"/$\" | awk '{ print $4 \" \" }'", 60, 0},
+        {" ", "volume", 0, 2},
+        {"  ", "df -h | grep \"/$\" | awk '{ print $4 \" \" }'", 60, 0},
 
         {" ", "battery_status BAT0", 5, 0},
         {" ", "battery_status BAT1", 5, 0},
